9bb743ccbfd9aee1a7ccbe9f88be94e027a23903
Alex Chirea <ac.chirea@gmail.com>
2020-12-19 20:03:35 +0200

    Initial commit

---
+++ src/main/java/com/alexchirea/SimpleSpringBootApp/SimpleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.alexchirea.SimpleSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexchirea/SimpleSpringBootApp/SimpleSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexchirea.SimpleSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
